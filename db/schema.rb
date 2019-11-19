121668279e89768e0018d4ff3c30252bdc190166
Zain <elabedeen.zain@gmail.com>
2019-11-20 00:36:57 +0100

    Add accounts table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_19_213802) do
+ActiveRecord::Schema.define(version: 2019_11_19_213806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "accounts", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_accounts_on_user_id"
+  end
 
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "email"
@@ -38,4 +45,5 @@ ActiveRecord::Schema.define(version: 2019_11_19_213802) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "accounts", "users"
 end
---
+++ spec/factories/accounts.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: accounts
+#
+#  id         :uuid             not null, primary key
+#  user_id    :uuid
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_accounts_on_user_id  (user_id)
+#
+
+
+FactoryBot.define do
+  factory :account do
+    
+  end
+end
