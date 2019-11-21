9305dce6836d937d29f77be38394cd300ee6948d
Zain <elabedeen.zain@gmail.com>
2019-11-21 20:51:00 +0100

    SignIn user

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,5 +1,7 @@
 module Types
   class MutationType < Types::BaseObject
-    field :create_user, mutation: Mutations::CreateUser
+    # Users
+    field :create_user, mutation: Mutations::Users::CreateUser
+    field :sign_in, mutation: Mutations::Users::SignIn
   end
 end
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,6 +1,7 @@
 module Types
     class UserType < BaseObject
       field :id, ID, null: false
+      field :jti, String, null: false
       field :full_name, String, null: false
     end
   end
