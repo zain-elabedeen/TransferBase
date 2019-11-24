54e3fe59a1f869eaed789754cfcc7e2eca3c81f3
Zain <elabedeen.zain@gmail.com>
2019-11-24 13:52:44 +0100

    Add current_user mutation

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,5 +1,11 @@
 module Types
   class QueryType < Types::BaseObject
+    field :current_user, Types::UserType, null: true
+    # TODO: Move to user query module/class
+    def current_user
+      context[:current_user]
+    end
+
     field :list_users, [Types::UserType], null: true
     # TODO: paginate and move to user query module/class
     def list_users
