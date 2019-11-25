60fe8193ed843d6a536cd9f51a830bbe3508efb7
Zain <elabedeen.zain@gmail.com>
2019-11-25 12:29:15 +0100

    Update DB Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-gold = User.create(email: ENV['GOLD_EMAIL'], full_name: 'Gold Bank', password: '12345!Aa')
+gold = User.create(email: ENV['GOLD_EMAIL'], name: 'Gold Bank', password: '12345!Aa')
 money_machine = Account.create(user: gold)
 
 transfer = Transfer.create!(
@@ -8,18 +8,19 @@ transfer = Transfer.create!(
   target_currency: 'USD',
   exchange_rate: '0',
   amount: 10000000000,
-  status: success
+  status: 'success'
 )
 
 Payout.create!(
   transfer_id: transfer.id,
   account_id: transfer.sender_account_id,
   amount: transfer.amount,
-  currency: transfer.source_currency
+  currency: transfer.source_currency,
+  status: 'success'
 )
 
-user1 = User.create(email: 'user1@test.com',full_name: 'User 1', password: '12345!Aa')
+user1 = User.create(email: 'user1@test.com',name: 'User 1', password: '12345!Aa')
 Account.create(user: user1)
 
-user2 = User.create(email: 'user2@test.com',full_name: 'User 2', password: '12345!Aa')
+user2 = User.create(email: 'user2@test.com',name: 'User 2', password: '12345!Aa')
 Account.create(user: user2)
